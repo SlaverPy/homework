3f7337f91fc69d41ccec513d0c9e1155cecfcf9f
Valeriy Sinitskiy <108592837+SlaverPy@users.noreply.github.com>
2023-10-21 18:08:13 +0300

    scipy

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-import collections
+import numpy as np
+from scipy.optimize import minimize
 
+# Определим функцию, минимум которой мы хотим найти
+def func(x):
+    return x**2 + 2*x + 1
 
-def calculate_character_frequency(file_path):
-    # Используем collections.Counter для подсчета символов
-    character_count = collections.Counter()
+# Зададим начальное приближение
+x0 = 0
 
-    # Определите размер буфера (в байтах), чтобы читать файл по частям
-    buffer_size = 1024 * 1024  # 1 МБ, можно изменить по желанию
+# Используем функцию minimize из scipy.optimize для нахождения минимума функции
+result = minimize(func, x0)
 
-    with open(file_path, 'rb') as file:
-        while True:
-            data = file.read(buffer_size)
-            if not data:
-                break
-
-            # Преобразуем байты в строки и учитываем только буквы и цифры
-            data = data.decode('utf-8', errors='ignore')
-            data = ''.join(filter(str.isalnum, data))
-
-            # Обновляем счетчик символов
-            character_count.update(data)
-
-    total_characters = sum(character_count.values())
-
-    # Выводим результат
-    for char, count in character_count.items():
-        frequency = (count / total_characters) * 100
-        print(f'{char} — {frequency:.2f}%')
-
-
-if __name__ == "__main__":
-    file_path = 'путь_к_вашему_файлу.txt'  # Замените на путь к вашему файлу
-    calculate_character_frequency(file_path)
+# Выведем результат
+print(result.x)
